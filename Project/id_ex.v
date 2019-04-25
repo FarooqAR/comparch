92b89f7a50bd9d0e33f904b8378c5f63659225df
Farooq AR <farooqbckk@gmail.com>
2019-04-25 11:21:47 +0500

    add buffers

---
+++ Project/id_ex.v
@@ -0,0 +1,60 @@
+module ID_EX(
+  input clk,
+  
+  // data
+  input [63:0] Inst_Addr,
+  output reg [63:0] Inst_Addr_Out,
+  
+  input [4:0] rs1,
+  output reg [4:0] rs1_Out,
+  
+  input [4:0] rs2,
+  output reg [4:0] rs2_Out,
+  
+  input [4:0] rd,
+  output reg [4:0] rd_Out,
+  
+  input [63:0] ReadData1, 
+  output reg [63:0] ReadData1_Out,
+  
+  input [63:0] ReadData2,
+  output reg [63:0] ReadData2_Out,
+  
+  input [63:0] ImmediateData,
+  output reg [63:0] ImmediateData_Out,
+  
+  input [31:0] Instruction,
+  output [3:0] Funct, // Instruction [30, 14-12]
+
+  // control
+  input [1:0] WB,
+  output reg [1:0] WB_Out,
+  
+  input [2:0] M,
+  output reg [2:0] M_Out,
+  
+  input [2:0] EX,
+  output reg [1:0] ALUOp,
+  output reg ALUSrc
+);
+
+always @ (posedge clk)
+begin
+  // data
+  Inst_Addr_Out = Inst_Addr;
+  rs1_Out = rs1;
+  rs2_Out = rs2;
+  rd_Out = rd_Out;
+  ReadData1_Out = ReadData1;
+  ReadData2_Out = ReadData2;
+  ImmediateData_Out = ImmediateData;
+  Funct = {Instruction[30], Instruction[14:12]};
+  
+  // control
+  WB_Out = WB;
+  M_Out = M;
+  ALUOp = EX[1:0];
+  ALUSrc = EX[2];
+end
+
+endmodule
---
+++ Project/mem_wb.v
@@ -0,0 +1,26 @@
+module MEM_WB():
+(
+    input [1:0] WB,
+    output reg Mem_to_Reg, Reg_Write,
+    
+    input [63:0] Read_Data,
+    output reg [63:0] Read_Data_out,
+    
+    input [4:0] rd,
+    output reg [4:0] rd_out,
+
+    input [63:0] Mem_Address,
+    output reg [63:0] Mem_Address_out,
+
+);
+
+    always@(posedge clk)
+    begin
+      Mem_to_Reg = WB[0];
+      Reg_Write = WB[1];
+      
+      Read_Data_out = Read_Data;
+      rd_out = rd;
+    end
+
+endmodule
